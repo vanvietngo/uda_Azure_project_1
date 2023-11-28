ba1822e1af041f5b10ca75fec97b363b69269707
viet.ngovan <viet.ngovan@halliburton.com>
2023-11-28 23:39:06 +0700

    update app.logger

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,6 +11,10 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+app.logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.DEBUG)
+app.logger.addHandler(streamHandler)
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
